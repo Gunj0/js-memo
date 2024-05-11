592972abb8a433f15008ca7f84923e386a4fc75c
gunj0 <gunjo2012@gmail.com>
2024-05-11 23:56:53 +0900

    infrastructureへ変更

--- Reversi/src/application/turnService.ts
+++ Reversi/src/application/turnService.ts
@@ -1,5 +1,5 @@
-import { GameGateway } from "../dataaccess/gameGateway";
-import { connectMySql } from "../dataaccess/connection";
+import { GameGateway } from "../infrastructure/gameGateway";
+import { connectMySql } from "../infrastructure/connection";
 import { toDisc } from "../domain/turn/disc";
 import { Point } from "../domain/turn/point";
 import { TurnRepository } from "../domain/turn/turnRepository";
