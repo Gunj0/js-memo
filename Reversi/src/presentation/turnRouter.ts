fd01e55845dc9643bbc565e833ac89467c1414f5
gunj0 <gunjo2012@gmail.com>
2024-05-27 18:59:50 +0900

    サービスのユースケース化

--- Reversi/src/presentation/turnRouter.ts
+++ Reversi/src/presentation/turnRouter.ts
@@ -1,18 +1,26 @@
 import express from "express";
-import { TurnService } from "../application/service/turnService";
 import { Point } from "../domain/model/turn/point";
 import { toDisc } from "../domain/model/turn/disc";
 import { TurnMySQLRepository } from "../infrastructure/repository/turn/turnMySQLRepository";
 import { GameMySQLRepository } from "../infrastructure/repository/game/gameMySQLRepository";
 import { GameResultMySQLRepository } from "../infrastructure/repository/gameResult/gameResultMySQLRepository";
+import { RegisterTurnUseCase } from "../application/useCase/registerTurnUseCase";
+import { FindLatestGameTurnByTurnCountUseCase } from "../application/useCase/findLatestGameTurnByTurnCountUseCase";
 
 export const turnRouter = express.Router();
 
-const turnService = new TurnService(
+const registerTurnUseCase = new RegisterTurnUseCase(
   new TurnMySQLRepository(),
   new GameMySQLRepository(),
   new GameResultMySQLRepository()
 );
+
+const findLatestGameTurnByTurnCountUseCase =
+  new FindLatestGameTurnByTurnCountUseCase(
+    new TurnMySQLRepository(),
+    new GameMySQLRepository(),
+    new GameResultMySQLRepository()
+  );
 
 interface TurnGetResponseBody {
   turnCount: number;
@@ -36,7 +44,7 @@ turnRouter.get(
   async (req, res: express.Response<TurnGetResponseBody>) => {
     const turnCount = parseInt(req.params.turnCount);
 
-    const output = await turnService.findLatestGameTurnByTurnCount(turnCount);
+    const output = await findLatestGameTurnByTurnCountUseCase.run(turnCount);
 
     const responseBody = {
       turnCount: output.turnCount,
@@ -57,7 +65,7 @@ turnRouter.post(
     const disc = toDisc(req.body.move.disc);
     const point = new Point(req.body.move.x, req.body.move.y);
 
-    await turnService.registerTurn(turnCount, disc, point);
+    await registerTurnUseCase.run(turnCount, disc, point);
 
     res.status(201).end();
   }
