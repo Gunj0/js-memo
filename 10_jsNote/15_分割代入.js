22a709c14038761f941609e7780b528d90294d8d
Gunj0 <gunjo2012@gmail.com>
2024-11-04 15:20:35 +0900

    分割歳入

--- 10_jsNote/15_分割代入.js
+++ 10_jsNote/15_分割代入.js
@@ -12,6 +12,17 @@ const { name1 } = myProfile1; // 1つ目
 const { age1 } = myProfile1; // 2つ目
 // デフォルト値
 const { name3, age3, length3 = 150 } = myProfile1;
+// ネストプロパティの分割方法
+const Person = {
+  name: "bill",
+  job: {
+    jobName: "engineer",
+    jobNumber: 1,
+  },
+};
+const {
+  job: { jobName }, // 「jobName」変数が取り出せる
+} = Person;
 
 // 配列の分割代入(変数名は自由)
 const myProfile2 = ["山田", 20];
